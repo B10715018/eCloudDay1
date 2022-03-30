741f3deb659eac8459b1d0392eb7c74c58a835ef
B10715018 <brandon.wymer@rocketmail.com>
2022-03-31 01:33:01 +0800

    put some try and except clauses

--- sns-get-topic-attribute.py
+++ sns-get-topic-attribute.py
@@ -1,3 +1,4 @@
+from ast import Return
 import boto3
 import json
 
@@ -6,16 +7,23 @@ REGION_NAME = 'us-west-2'
 client = boto3.client('sns', region_name=REGION_NAME)
 
 # open file for searching all kind of sns topic
-with open('./data/sns-list-topic-'+REGION_NAME+'.json', 'r')as openfile:
-    json_object = json.load(openfile)
+try:
+    with open('./data/sns-list-topic-'+REGION_NAME+'.json', 'r')as openfile:
+        json_object = json.load(openfile)
 
-snsArnList = []
-# get all the sns topic in specified region
-for item in json_object['Topics']:
-  # get the topic attribute for each sns and write into json file
-    response = client.get_topic_attributes(
-        TopicArn=item['TopicArn'])
-    json_list = json.dumps(response)
-    with open('./data/sns-get-topic-attribute-'+item['TopicArn']+'.json', 'w') as outfile:
-        outfile.write(json_list)
-        outfile.close()
+    snsArnList = []
+    # get all the sns topic in specified region
+    for item in json_object['Topics']:
+        # get the topic attribute for each sns and write into json file
+        try:
+            response = client.get_topic_attributes(
+                TopicArn=item['TopicArn'])
+            json_list = json.dumps(response)
+            with open('./data/sns-get-topic-attribute-'+item['TopicArn']+'.json', 'w') as outfile:
+                outfile.write(json_list)
+                outfile.close()
+        except:
+            print('Something is Error')
+
+except:
+    print('File not found')
