ec85c80e317ee3fce23ddfcd222cfe652ff4d379
B10715018 <brandon.wymer@rocketmail.com>
2022-03-31 01:15:37 +0800

    Update dynamodb-scan.py

--- dynamodb-scan.py
+++ dynamodb-scan.py
@@ -1,23 +1,23 @@
 import boto3
 import json
 
-client=boto3.client('dynamodb')
-import boto3
+REGION_NAME = 'us-west-2'
+client = boto3.client('dynamodb', region_name=REGION_NAME)
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 
 table = dynamodb.Table('translate')
 
 response = table.scan()
 
-existingKeys=[]
+existingKeys = []
 
 for item in response['Items']:
-  for field in item.keys():
-    if field not in existingKeys:
-      existingKeys.append(field)
+    for field in item.keys():
+        if field not in existingKeys:
+            existingKeys.append(field)
 
-response['Items']=existingKeys
-json_string=json.dumps(response)
+response['Items'] = existingKeys
+json_string = json.dumps(response)
 
-with open ('./data/dynamoDB-item-list-scan.json','w') as outfile:
-  outfile.write(json_string)
+with open('./data/dynamoDB-item-list-scan.json', 'w') as outfile:
+    outfile.write(json_string)
