5704a1340030589cd5843d0b058a15808b0d2c5b
B10715018 <brandon.wymer@rocketmail.com>
2022-03-31 17:33:52 +0800

    fix file name

--- ec2-describe-subent.py
+++
@@ -1,9 +0,0 @@
-import boto3
-import json
-import datetime
-client=boto3.client('ec2',region_name='us-west-2')
-response = client.describe_subnets()
-json_list=json.dumps(response)
-with open('./data/ec2-describe-subent.json','w') as outfile:
-  outfile.write(json_list)
-  outfile.close()
