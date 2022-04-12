b47b36cf307d7784e4e091d79087bcec42279859
chen <cindy31224@gmail.com>
2022-04-12 10:50:16 +0800

    Update rds-describe-instance.py
    fix hard coded

--- rds-describe-instance.py
+++ rds-describe-instance.py
@@ -1,7 +1,8 @@
 import boto3
 import json
 
-client=boto3.client('rds',region_name='us-west-2')
+REGION_NAME = 'us-west-2'
+client=boto3.client('rds',region_name=REGION_NAME)
 response =client.describe_db_instances()
 json_list=json.dumps(response)
 with open('./data/rds-describe-instance.json','w') as outfile:
