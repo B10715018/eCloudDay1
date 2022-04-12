16c81c95e48fadd9cb47aeb54b5bd5bdb8621a37
chen <cindy31224@gmail.com>
2022-04-12 12:54:42 +0800

    Update elbv2-describe-load-balancer.py
    fix hard coded

--- elbv2-describe-load-balancer.py
+++ elbv2-describe-load-balancer.py
@@ -1,19 +1,26 @@
 import boto3
 import json
-client = boto3.client('elbv2', region_name='us-west-2')
-response = client.describe_load_balancers(
-)
-
-dateList = []
-count = 0
-for item in response['LoadBalancers']:
-    count += 1
-    dateList.append(json.dumps(item['CreatedTime'], default=str))
-
-for i in range(count):
-    response['LoadBalancers'][i]['CreatedTime'] = dateList[i]
-
-json_list = json.dumps(response)
-with open('./data/elbv2-describe-load-balancer'+'.json', 'w')as outfile:
-    outfile.write(json_list)
-    outfile.close()
+REGION_NAME = 'us-west-2'
+client = boto3.client('elbv2', region_name=REGION_NAME)
+try:
+    f = open('./data/elbv2-describe-load-balancer.json')
+    data = json.load(f)
+    count = 0
+    TargetGroupArnList = []
+    for item in data['TargetGroups']:
+        count += 1
+        TargetGroupArnList.append(item['TargetGroupArn'])
+        print(TargetGroupArnList)
+    try:
+        for i in range(count):
+            response = client.describe_target_health(
+                TargetGroupArn=TargetGroupArnList[i]
+            )
+        json_list = json.dumps(response)
+        with open('./data/elbv2-describe-target-health'+'.json', 'w')as outfile:
+            outfile.write(json_list)
+            outfile.close()
+    except:
+        print('Error in elbv2-describe-target-health')
+except:
+    print('File not found for elbv2-describe-target-health')
