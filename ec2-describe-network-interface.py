b72f0ac5cd8183e3ad0aff3f2e8e4e73eb7da610
chen <cindy31224@gmail.com>
2022-04-12 13:31:25 +0800

    fix hard coded

--- ec2-describe-network-interface.py
+++ ec2-describe-network-interface.py
@@ -1,13 +1,17 @@
 import boto3
 import json
 import datetime
-
-client=boto3.client('ec2',region_name='us-west-2')
+REGION_NAME = 'us-west-2'
+client = boto3.client('ec2', region_name=REGION_NAME)
 response = client.describe_network_interfaces()
+
+
 def defaultconverter(o):
-  if isinstance(o, datetime.datetime):
-      return o.__str__()
-json_list=json.dumps(response,default = defaultconverter)
-with open('./data/ec2-describe-network-interface.json','w') as outfile:
-  outfile.write(json_list)
-  outfile.close()
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+
+json_list = json.dumps(response, default=defaultconverter)
+with open('./data/ec2-describe-network-interface.json', 'w') as outfile:
+    outfile.write(json_list)
+    outfile.close()
