7fd9ee60d315b99d7383398a9bc7c18656470b53
chen <cindy31224@gmail.com>
2022-04-13 13:08:19 +0800

    Update cloudwatch-describe-log-stream.py

--- cloudwatch-describe-log-stream.py
+++ cloudwatch-describe-log-stream.py
@@ -2,24 +2,32 @@ import boto3
 import json
 REGION_NAME = 'us-west-2'
 client = boto3.client('logs', region_name=REGION_NAME)
-f = open('./data/cloudwatch-describe-log-groups.json')
-data = json.load(f)
-count = 0
-logGroupNameList = []
-for item in data['logGroups']:
-    count += 1
-    logGroupNameList.append(item['logGroupName'])
+try:
+    f = open('./data/cloudwatch-describe-log-groups.json')
+    data = json.load(f)
+    count = 0
+    logGroupNameList = []
+    for item in data['logGroups']:
+        count += 1
+        logGroupNameList.append(item['logGroupName'])
 
-for i in range(count):
-    response = client.describe_log_streams(
-        logGroupName=logGroupNameList[i]
-    )
-    json_list = json.dumps(response)
+    for i in range(count):
+        response = client.describe_log_streams(
+            logGroupName=logGroupNameList[i],
+            orderBy='LastEventTime',
+            limit=1
+            )
+        json_list = json.dumps(response)
 
-    str_logGroupName = "".join(logGroupNameList[i])
-    new_logGroupName = str_logGroupName.replace('/', '-')
-    list(new_logGroupName)
+        str_logGroupName = "".join(logGroupNameList[i])
+        new_logGroupName = str_logGroupName.replace('/', '-')
+        list(new_logGroupName)
 
-    with open('./data/cloudwatch-log-stream/cloudwatch-describe-log-streams'+new_logGroupName+'.json', 'w')as outfile:
-        outfile.write(json_list)
-        outfile.close()
+        with open('./data/cloudwatch-log-stream/cloudwatch-describe-log-stream'+new_logGroupName+'.json', 'w')as outfile:
+            outfile.write(json_list)
+            outfile.close()
+except:
+    print("File not found for cloudwatch-describe-log-stream")
+
+    
+
