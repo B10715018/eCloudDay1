13996aba1cc5bfbdc4af14863b761f222f14b715
Sing-JieWu <66322621+Sing-JieWu@users.noreply.github.com>
2022-04-14 16:46:03 +0800

    Update dynamodb_list_table.py

--- utils/dynamodb_list_table.py
+++ utils/dynamodb_list_table.py
@@ -11,11 +11,11 @@ def dynamodb_list_table():
         response = client.list_tables()
         json_response = json.dumps(response)
         file_path = os.path.join(
-        script_dir, 'data/dynamodb-list.json')
+        script_dir, 'data/dynamodb-list-table'+'.json')
         with open(file_path, 'w') as outfile:
             outfile.write(json_response)
             outfile.close()
- 
-
     except:
         print('File not found for dynamodb-list-table')
+
+dynamodb_list_table()
