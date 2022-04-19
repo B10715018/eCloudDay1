8bff118b9d440af19b4f7f5e1f0d6c6f61055429
B10715018 <brandon.wymer@rocketmail.com>
2022-04-19 18:21:56 +0800

    delete all unused python file

--- ec2-describe-subnet.py
+++
@@ -1,9 +0,0 @@
-import boto3
-import json
-REGION_NAME = 'us-west-2'
-client = boto3.client('ec2', region_name=REGION_NAME)
-response = client.describe_subnets()
-json_list = json.dumps(response)
-with open('./data/ec2-describe-subnet.json', 'w') as outfile:
-    outfile.write(json_list)
-    outfile.close()
