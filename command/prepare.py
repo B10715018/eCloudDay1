7ccc1a3efde4100f4567ec0bee14a06efee9e3c7
B10715018 <brandon.wymer@rocketmail.com>
2022-04-20 13:27:29 +0800

    refactor lambda to translate edge

--- command/prepare.py
+++ command/prepare.py
@@ -10,6 +10,7 @@ from utils.translate_prepare_node import translate_prepare_node
 from utils.sns_prepare_node import sns_prepare_node
 from utils.sfn_prepare_node import sfn_prepare_node
 from utils.edge_lambda_to_transcribe import edge_lambda_to_transcribe
+from utils.edge_lambda_to_translate import edge_lambda_to_translate_find
 
 REGION_NAME = 'us-west-2'
 ACCOUNT_ID = '758325631830'
@@ -66,6 +67,10 @@ class Prepare:
         edge_lambda_to_transcribe(
             self.cytoscape_edge_data, self.region_name, self.account_id)
 
+    def find_edge_lambda_to_translate(self):
+        edge_lambda_to_translate_find(
+            self.cytoscape_edge_data, self.region_name, self.account_id)
+
 
 # initialize a class
 prepare_command = Prepare(REGION_NAME, ACCOUNT_ID)
@@ -85,38 +90,11 @@ prepare_command.find_sfn_connection()
 prepare_command.find_edge_lambda_to_sns()
 prepare_command.find_edge_lambda_to_ddb()
 prepare_command.find_edge_lambda_to_transcribe()
+prepare_command.find_edge_lambda_to_translate()
 # EXPORT INTO JSON FILE
 prepare_command.exportToJSON()
 
 # '''STARTING FROM HERE IS LOGIC TO FIND RELATIONSHIPS'''
-
-# '''FIND CONNECTION BETWEEN LAMBDA AND TRANSCRIBE'''
-# list_of_files = os.listdir('./data')  # list of files in the data directory
-# for each_file in list_of_files:
-#     # since its all type str you can simply use startswith
-#     if each_file.startswith('cloudtrail-start-transcription'):
-#         with open('./data/' + each_file, 'r') as openfile:
-#             cloudtrail_transcribe_object = json.load(openfile)
-#             try:
-#                 if 'AWS_Lambda' in cloudtrail_transcribe_object['userAgent']:
-#                     lambda_arn = 'arn:aws:lambda:'+REGION_NAME+':'+ACCOUNT_ID + \
-#                         ':function:' + \
-#                         (cloudtrail_transcribe_object['userIdentity']
-#                          ['principalId'].split(':')[-1])
-#                     transcribe_edge_data = {
-#                         "data": {
-#                             "id": lambda_arn+'-'+'transcribe',
-#                             "source": lambda_arn,
-#                             "target": 'transcribe',
-#                         }
-#                     }
-#                     if(cytoscape_edge_data.count(transcribe_edge_data) == 0):
-#                         cytoscape_edge_data.append(transcribe_edge_data)
-#                     print('Found connection between transcribe and lambda:{}'.format(
-#                         cloudtrail_transcribe_object['userIdentity']['principalId'].split(':')[-1]))
-#             except:
-#                 print('No connection between lambda and transcribe')
-#         openfile.close()
 
 # '''FIND CONNECTION BETWEEN TRANSCRIBE AND S3'''
 # for each_file in list_of_files:
@@ -144,36 +122,6 @@ prepare_command.exportToJSON()
 #                 ))
 #         openfile.close()
 
-# '''FIND CONNECTION BETWEEN LAMBDA AND TRANSLATE'''
-# list_of_files = os.listdir('./data')  # list of files in the data directory
-# for each_file in list_of_files:
-#     # since its all type str you can simply use startswith
-#     if each_file.startswith('cloudtrail-translate-text-'):
-#         with open('./data/' + each_file, 'r') as openfile:
-#             cloudtrail_translate_object = json.load(openfile)
-#             try:
-#                 if 'AWS_Lambda' in cloudtrail_translate_object['userAgent']:
-#                     lambda_arn = 'arn:aws:lambda:'+REGION_NAME+':'+ACCOUNT_ID + \
-#                         ':function:' + \
-#                         (cloudtrail_translate_object['userIdentity']
-#                          ['principalId'].split(':')[-1])
-#                     translate_edge_data = {
-#                         "data": {
-#                             "id": lambda_arn+'-'+'translate',
-#                             "source": lambda_arn,
-#                             "target": 'translate',
-#                         }
-#                     }
-#                     if(cytoscape_edge_data.count(translate_edge_data) == 0):
-#                         cytoscape_edge_data.append(translate_edge_data)
-#                     print('Found connection between translate and lambda:{}'.format(
-#                         cloudtrail_translate_object['userIdentity']['principalId'].split(':')[-1]))
-#             except:
-#                 print('No connection between lambda and translate')
-#         openfile.close()
-
-# filtered_cytoscape_data.append(cytoscape_node_data)
-# filtered_cytoscape_data.append(cytoscape_edge_data)
 
 '''FIND CONNECTION FROM LAMBDA TO STEP FUNCTION'''
 '''FIND CONNECTION APIGW TO STEP FUNCTION'''
