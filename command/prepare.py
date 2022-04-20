0a9124f8238956d26dd494d35f855d15e7e7e68c
B10715018 <brandon.wymer@rocketmail.com>
2022-04-20 14:57:38 +0800

    refactor apigw node

--- command/prepare.py
+++ command/prepare.py
@@ -12,6 +12,7 @@ from utils.sfn_prepare_node import sfn_prepare_node
 from utils.edge_lambda_to_transcribe import edge_lambda_to_transcribe
 from utils.edge_lambda_to_translate import edge_lambda_to_translate_find
 from utils.edge_transcribe_to_s3_find import edge_transcribe_to_s3_find
+from utils.apigw_prepare_node import api_gw_prepare_node
 
 REGION_NAME = 'us-west-2'
 ACCOUNT_ID = '758325631830'
@@ -55,6 +56,10 @@ class Prepare:
         sfn_prepare_node(self.region_name, self.account_id,
                          self.cytoscape_node_data)
 
+    def prepare_apigw_node(self):
+        api_gw_prepare_node(self.region_name, self.account_id,
+                            self.cytoscape_node_data)
+
     def find_sfn_connection(self):
         sfn_find_connection(self.cytoscape_node_data, self.cytoscape_edge_data)
 
@@ -90,6 +95,7 @@ prepare_command.prepare_transcribe_node()
 prepare_command.prepare_translate_node()
 prepare_command.prepare_sns_node()
 prepare_command.prepare_sfn_node()
+prepare_command.prepare_apigw_node()
 # prepare edge logic
 prepare_command.find_sfn_connection()
 prepare_command.find_edge_lambda_to_sns()
@@ -100,37 +106,8 @@ prepare_command.find_edge_transcribe_to_s3()
 # EXPORT INTO JSON FILE
 prepare_command.exportToJSON()
 
-# '''STARTING FROM HERE IS LOGIC TO FIND RELATIONSHIPS'''
-
-# '''FIND CONNECTION BETWEEN TRANSCRIBE AND S3'''
-# for each_file in list_of_files:
-#     # since its all type str you can simply use startswith
-#     if each_file.startswith('cloudtrail-start-transcription'):
-#         with open('./data/' + each_file, 'r') as openfile:
-#             cloudtrail_transcribe_object = json.load(openfile)
-#             try:
-#                 for s3 in s3_object['Buckets']:
-#                     if(cloudtrail_transcribe_object['requestParameters']['outputBucketName'] == s3['Name']):
-#                         transcribe_s3_edge_data = {
-#                             "data": {
-#                                 "id": 'transcribe-'+'s3:'+s3['Name'],
-#                                 "source": 'transcribe',
-#                                 "target": 's3:'+s3['Name'],
-#                             }
-#                         }
-#                         if(cytoscape_edge_data.count(transcribe_s3_edge_data) == 0):
-#                             cytoscape_edge_data.append(transcribe_s3_edge_data)
-#                             print('Found connection between transcribe and s3:{}'.format(
-#                                 cloudtrail_transcribe_object['requestParameters']['outputBucketName']))
-#             except:
-#                 print('No connection between transcribe and s3 bucket:{}'.format(
-#                     cloudtrail_transcribe_object['requestParameters']['outputBucketName']
-#                 ))
-#         openfile.close()
-
 
 '''FIND CONNECTION FROM LAMBDA TO STEP FUNCTION'''
 '''FIND CONNECTION APIGW TO STEP FUNCTION'''
 '''FIND CONNECTION S3 TO COGNITO'''
-'''PREPARE FOR APIGW'''
 '''FIND CONNECTION FROM SNS TO LAMBDA'''
