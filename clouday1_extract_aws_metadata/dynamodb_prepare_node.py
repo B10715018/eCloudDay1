153a8f96b722c4969a471282c7f6385abe90f08b
chen31224 <75403996+chen31224@users.noreply.github.com>
2022-05-04 18:18:38 +0800

    Update dynamodb_prepare_node.py (#100)
    * Update dynamodb_prepare_node.py
    
    add partition_key into node data
    
    * Update dynamodb_prepare_node.py
    
    * fix bug in prepare dynamodb node
    
    Co-authored-by: Brandon Wymer Pramana <72436395+B10715018@users.noreply.github.com>

--- clouday1_extract_aws_metadata/dynamodb_prepare_node.py
+++ clouday1_extract_aws_metadata/dynamodb_prepare_node.py
@@ -15,15 +15,21 @@ def dynamodb_prepare_node(region, account_id, cytoscape_node_data):
                 script_dir, 'data/dynamodb-table/dynamoDB-item-list-scan-'+region+'-'+item+'.json')
             with open(file_path_read_ddb, 'r')as openfile_ddb:
                 ddb_items = json.load(openfile_ddb)
-                cytoscape_node_data.append({
-                    "data": {
-                        "type": "dynamodb",
-                        "id": 'ddb:'+item,
-                        "region": region,
-                        "name": item,
-                        "account_id": account_id,
-                        "itemCount": ddb_items["Count"],
-                        "items": ddb_items["Items"]
-                    }
-                })
+            file_path_describe_ddb = os.path.join(
+                script_dir, 'data/ddb-describe-table/dynamodb-describe-table-'+item+'-'+region+'.json')
+            with open(file_path_describe_ddb,'r')as openfile_describe:
+                ddb_table=json.load(openfile_describe)
+
+            cytoscape_node_data.append({
+                "data": {
+                    "type": "dynamodb",
+                    "id": 'ddb:'+item,
+                    "region": region,
+                    "name": item,
+                    "account_id": account_id,
+                    "itemCount": ddb_items["Count"],
+                    "partition_key": ddb_table["Table"]["KeySchema"][0]["AttributeName"],
+                    "items": ddb_items["Items"]
+                }
+            })
         openfile.close()
