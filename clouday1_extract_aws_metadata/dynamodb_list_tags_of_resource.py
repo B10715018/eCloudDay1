8f0270982943b1686457f087faea54ced81aa5c6
servtare <66322621+servtare@users.noreply.github.com>
2022-05-20 17:44:33 +0800

    Create dynamodb_list_tags_of_resource.py (#129)
    * add tags to apigw_prepare_node.py
    
    * modify cognito tags name in cognito_prepare_node.py
    
    * Create dynamodb_list_tags_of_resource.py
    
    Co-authored-by: Sing-JieWu <66322621+Sing-JieWu@users.noreply.github.com>
    Co-authored-by: Brandon Wymer Pramana <72436395+B10715018@users.noreply.github.com>

---
+++ clouday1_extract_aws_metadata/dynamodb_list_tags_of_resource.py
@@ -0,0 +1,30 @@
+import os
+import boto3
+import json
+
+def list_tags_of_resource(region, account_id):
+    client = boto3.client('dynamodb', region_name=region)
+    TableNameList = []
+    count = 0
+    script_dir = os.path.dirname('.')
+    file_path = os.path.join(
+        script_dir, 'data/dynamodb-list-table-'+region+'.json')
+    f = open(file_path, 'r')
+    data = json.load(f)
+
+    for item in data['TableNames']:
+        TableNameList.append(item)
+        count += 1
+    for i in range(count):
+
+        #get tags for each ddb table
+        response = client.list_tags_of_resource(
+            ResourceArn="arn:aws:dynamodb:"+region+":"+account_id+":table/"+TableNameList[i]
+        )
+
+        json_list = json.dumps(response, indent=4)
+        file_path2 = os.path.join(
+            script_dir, 'data/ddb-list-tags/dynamodb-list-tags-of-resource-'+TableNameList[i]+'-'+region+'.json')
+        with open(file_path2, 'w')as outfile:
+            outfile.write(json_list)
+            outfile.close()
---
+++ data/ddb-list-tags/ddb-list-tags.md
@@ -0,0 +1,4 @@
+This file is used for storing dynamodb tags information.
+
+[]: # Language: markdown
+[]: # Path: data/ddb-list-tags/ddb-list-tags.md
