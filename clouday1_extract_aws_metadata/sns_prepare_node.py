d248f662a8fa9797214f1743b31ec4656e50536c
Brandon Wymer Pramana <72436395+B10715018@users.noreply.github.com>
2022-05-24 17:40:53 +0800

    Update sns_prepare_node.py

--- clouday1_extract_aws_metadata/sns_prepare_node.py
+++ clouday1_extract_aws_metadata/sns_prepare_node.py
@@ -29,6 +29,7 @@ def sns_prepare_node(region, account_id, cytoscape_node_data):
                     "account_id": account_id,
                     "region": region,
                     "name": sns_name,
+                    "cost_for_month": "1.50 USD",
                     "tag": sns_tag
                 }
             })
