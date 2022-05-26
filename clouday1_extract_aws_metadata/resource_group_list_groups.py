d7b65251feadc66fabceeeae7a68c7bb7f5e653a
chen <cindy31224@gmail.com>
2022-05-26 15:45:12 +0800

    Create resource_group_list_groups.py

---
+++ clouday1_extract_aws_metadata/resource_group_list_groups.py
@@ -0,0 +1,16 @@
+import boto3
+import json
+import os
+
+
+def resource_group_list_groups(region):
+    client = boto3.client('resource-groups', region_name=region)
+    response = client.list_groups()
+
+    json_list = json.dumps(response)
+    script_dir = os.path.dirname('.')
+    file_path_write = os.path.join(
+        script_dir, 'data/resource_group_list_groups'+region+'.json')
+    with open(file_path_write, 'w')as outfile:
+        outfile.write(json_list)
+        outfile.close()
