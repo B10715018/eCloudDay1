5682315605f5b30687072a952c21c4c230c4099f
servtare <66322621+servtare@users.noreply.github.com>
2022-05-31 14:24:42 +0800

    Create elbv2_list_tags.py (#141)
    * add tags to apigw_prepare_node.py
    
    * modify cognito tags name in cognito_prepare_node.py
    
    * Create dynamodb_list_tags_of_resource.py
    
    * Create s3_get_bucket_tagging.py
    
    * Create lambda_list_tags.py
    
    * Create rds_prepare_node.py, Modify rds_describe_instance.py
    
    * Create elbv2_list_tags.py
    
    * Update elbv2_list_tags.py
    
    Co-authored-by: Sing-JieWu <66322621+Sing-JieWu@users.noreply.github.com>
    Co-authored-by: Brandon Wymer Pramana <72436395+B10715018@users.noreply.github.com>

---
+++ clouday1_extract_aws_metadata/elbv2_list_tags.py
@@ -0,0 +1,32 @@
+import boto3
+import json
+import os
+
+def elb_list_tags(region,AWS_ACCESS_KEY,AWS_SECRET_KEY):
+    client = boto3.client('elbv2', region_name=region,
+    aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_SECRET_KEY)
+    ElbArnList = []
+    ElbNameList = []
+    count = 0
+    script_dir = os.path.dirname('.')
+    file_path = os.path.join(
+        script_dir, 'data/elbv2-describe-load-balancer-'+region+'.json')
+    f = open(file_path, 'r')
+    data = json.load(f)
+
+    for item in data['LoadBalancers']:
+        ElbArnList.append(item['LoadBalancerArn'])
+        ElbNameList.append(item['LoadBalancerName'])
+        count += 1
+    for i in range(count):
+        #get tags for each elb
+        response = client.describe_tags(
+             ResourceArns=[ElbArnList[i],]
+        )
+        json_list = json.dumps(response["TagDescriptions"], indent=4)
+        file_path2 = os.path.join(
+            script_dir, 'data/elbv2-list-tags/elbv2-list-tags-'+ElbNameList[i]+"-"
+            +region+'.json')
+        with open(file_path2, 'w')as outfile:
+            outfile.write(json_list)
+            outfile.close()
