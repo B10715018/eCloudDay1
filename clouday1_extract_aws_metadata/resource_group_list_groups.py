c2905bae81d2ab2bab11447ad0d33a2c2f44293f
chen31224 <75403996+chen31224@users.noreply.github.com>
2022-05-31 13:50:25 +0800

    Create resource_group_list_groups.py (#139)
    * Create resource_group_list_groups.py
    
    * Update resource_group_list_groups.py
    
    fix output json file name
    
    * create resource-group-list-group-resources.py
    
    add data folder to store group resources
    
    * fix credential problem
    
    Co-authored-by: Brandon Wymer Pramana <72436395+B10715018@users.noreply.github.com>

---
+++ clouday1_extract_aws_metadata/resource_group_list_groups.py
@@ -0,0 +1,17 @@
+import boto3
+import json
+import os
+
+
+def resource_group_list_groups(region,AWS_ACCESS_KEY,AWS_SECRET_KEY):
+    client = boto3.client('resource-groups', region_name=region,
+    aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_SECRET_KEY)
+    response = client.list_groups()
+
+    json_list = json.dumps(response)
+    script_dir = os.path.dirname('.')
+    file_path_write = os.path.join(
+        script_dir, 'data/resource-group-list-groups-'+region+'.json')
+    with open(file_path_write, 'w')as outfile:
+        outfile.write(json_list)
+        outfile.close()
