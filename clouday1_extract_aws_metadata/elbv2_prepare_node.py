6c5b8fdb50737c0a4ab94c385aa04e3d699b5a03
Brandon Wymer Pramana <72436395+B10715018@users.noreply.github.com>
2022-06-01 14:25:01 +0800

    [feature] Halfway WAF feature (#152)
    * elb prepare node done
    
    * halfway waf feature

--- clouday1_extract_aws_metadata/elbv2_prepare_node.py
+++ clouday1_extract_aws_metadata/elbv2_prepare_node.py
@@ -11,45 +11,33 @@ def elbv2_prepare_node(region, account_id, cytoscape_node_data):
     with open(file_path_read, 'r') as openfile:
         elb_object = json.load(openfile)
         openfile.close()
-    
-    file_path_read = os.path.join(
-        script_dir, 'data/elbv2-describe-load-balancer-'+region+'.json')
-    with open(file_path_read, 'r') as openfile:
-        elb_object = json.load(openfile)
-        openfile.close()
-
-    list_of_files = os.listdir('./data/elbv2-list-tags')
-    for each_file in list_of_files:
-        # since its all type str you can simply use startswith
-        if each_file.startswith('elbv2-list-tags-'):
-          # open every file in elbv2-list-tags folder inside data folder
-            script_dir = os.path.dirname('.')
-            file_path_read_2 = os.path.join(
-                script_dir, 'data/elbv2-list-tags/'+each_file)
-            with open(file_path_read_2, 'r') as openfile:
-                elb_tag_object = json.load(openfile)
-                openfile.close()
 
         for item in elb_object['LoadBalancers']:
             az_list = [x['ZoneName'] for x in item['AvailabilityZones']]
+            each_file = 'elbv2-list-tags-' + \
+                item['LoadBalancerName']+'-'+region+'.json'
+            file_path_read_2 = os.path.join(
+                script_dir, 'data/elbv2-list-tags/'+each_file)
+            f = open(file_path_read_2, 'r')
+            elb_tag_object = json.load(f)
             elb_tag = {}
             for tag in elb_tag_object[0]['Tags']:
-                elb_tag[tag['Key']]=tag['Value']
+                elb_tag[tag['Key']] = tag['Value']
 
-        cytoscape_node_data.append({
-            "data": {
-                "type" : "ElbV2",
-                "id" : item['LoadBalancerArn'],
-                "arn" : item['LoadBalancerArn'],
-                "name" : item['LoadBalancerName'],
-                "dns" : item['DNSName'],
-                "account_id" : account_id,
-                "region" : region,
-                "vpc_id" : item['VpcId'],
-                "created_time" : item['CreatedTime'],
-                "lodabalancer_type" : item['Type'],
-                "availability_zones" :az_list,
-                "tag":elb_tag,
-                "cost_for_month" : 4.91
+            cytoscape_node_data.append({
+                "data": {
+                    "type": "ElbV2",
+                    "id": item['LoadBalancerArn'],
+                    "arn": item['LoadBalancerArn'],
+                    "name": item['LoadBalancerName'],
+                    "dns": item['DNSName'],
+                    "account_id": account_id,
+                    "region": region,
+                    "vpc_id": item['VpcId'],
+                    "created_time": item['CreatedTime'],
+                    "lodabalancer_type": item['Type'],
+                    "availability_zones": az_list,
+                    "tag": elb_tag,
+                    "cost_for_month": 4.91
                 }
             })
