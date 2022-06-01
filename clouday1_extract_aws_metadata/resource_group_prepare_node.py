d843f6889641356ad9d5d6c8d3dfb13edd8f8f3b
chen31224 <75403996+chen31224@users.noreply.github.com>
2022-06-01 13:34:51 +0800

    Update resource group prepare node.py (#151)
    * Create resource_group_prepare_node.py
    
    * fix-cost
    
    * Update resource_group_prepare_node.py
    
    * Update resource_group_prepare_node.py
    
    Co-authored-by: B10715018 <brandon.wymer@rocketmail.com>

--- clouday1_extract_aws_metadata/resource_group_prepare_node.py
+++ clouday1_extract_aws_metadata/resource_group_prepare_node.py
@@ -6,18 +6,24 @@ def resource_group_prepare_node(region, account_id, cytoscape_node_data):
     '''Prepare for resource group'''
     script_dir = os.path.dirname('.')
     file_path_read = os.path.join(
-        script_dir, 'data/resource-group-list-groups--'+region+'.json')
+        script_dir, 'data/resource-group-list-groups-'+region+'.json')
     with open(file_path_read, 'r') as openfile:
         resource_group_object = json.load(openfile)
         openfile.close()
-        count=0
-        groupARN=[]
-        groupName=[]
         for item in resource_group_object['GroupIdentifiers']:
-            groupARN.append(item['GroupArn'])
-            groupName.append(item['GroupName'])
-            count+=1
-        for i in range(count):
+            groupARN = (item['GroupArn'])
+            groupName = (item['GroupName'])
+            file_path_read_tag = os.path.join(script_dir,
+                                              'data/resource-group-list-tags/resource-group-list-tags-'+region+'-'+groupName+'.json')
+            resource_group_tag_object = {}
+            try:
+                with open(file_path_read_tag, 'r') as openfile_tag:
+                    resource_group_tag_object = json.load(openfile_tag)
+                    openfile_tag.close()
+                    resource_group_tag = resource_group_tag_object['Tags']
+            except:
+                print('file not exist')
+
             cytoscape_node_data.append({
                 "data": {
                     "type": "Resource-Group",
@@ -26,6 +32,7 @@ def resource_group_prepare_node(region, account_id, cytoscape_node_data):
                     "account_id": account_id,
                     "region": region,
                     "name": groupName,
+                    "tag": resource_group_tag,
                     "cost_for_month": 0.00
-                    }
-                })
+                }
+            })
