62868eb7faf620df5312fd0a52f16a32e9944d18
Brandon Wymer Pramana <72436395+B10715018@users.noreply.github.com>
2022-06-08 19:44:33 +0800

    vpc and subnet functionality done

---
+++ clouday1_extract_aws_metadata/ec2_describe_vpcs.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+import datetime
+import os
+
+
+def defaultconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+
+def ec2_describe_vpcs(region,AWS_ACCESS_KEY,AWS_SECRET_KEY):
+    client = boto3.client('ec2', region_name=region,
+    aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_SECRET_KEY)
+    response = client.describe_vpcs()
+
+    json_list = json.dumps(response, default=defaultconverter)
+    script_dir = os.path.dirname('.')
+    file_path_write = os.path.join(
+        script_dir, 'data/ec2-describe-vpcs-'+region+'.json')
+    with open(file_path_write, 'w') as outfile:
+        outfile.write(json_list)
+        outfile.close()
